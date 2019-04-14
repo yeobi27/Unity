a087270ad81eaf0f3a8721e8865424d77c6281cb
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-15 01:11:15 +0900

    Roll a Ball

---
+++ ALIVE/Assets/Scripts/Player.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameManager.instance.playersTurn)
+        {
+            return;
+        }
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if(horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if(other.tag == "Food")
+        {
+            food = food + pointsPerFood;
+            other.gameObject.SetActive(false);
+        }
+        else if(other.tag == "Soda")
+        {
+            food = food + pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+
+        //throw new System.NotImplementedException();
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food = food - loss;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
+}
---
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //// Active before Renderring : gaming code
+    //void Update()
+    //{
+
+    //}
+    public float speed;
+
+    private Rigidbody rb;
+
+    private int count;
+
+    public Text countText;
+
+    public Text WinText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        WinText.text = "";
+        SetCountText();
+    }
+
+    // Active before phisics calc : phisics code
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            WinText.text = "You Win";
+        }
+    }
+}
+
